2c21494e9015164c6643198109a8a138dbeb65ae
Jack Atherton <jackrrye@gmail.com>
2019-05-11 18:51:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,17 @@
 # Add  code here!
 def prime?(x)
-  for n in 1..(x-1)
-  
+  number_of_factors = 0 
+  for n in 2..(x-1)
+    z = x % n 
+    if z > 0 
+      number_of_factors += 1 
+    else 
+      number_of_factors
+    end
+  end 
+  if number_of_factors > 0 
+    false 
+  else 
+    true 
+  end
+end
